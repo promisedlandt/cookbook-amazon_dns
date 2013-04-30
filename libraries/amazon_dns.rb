e12204e96c80787efa57f0ee1715855712760ab2
Nils Landt <nl@fugue>
2013-04-30 05:36:24 +0200

    Fix 'SSL_write:: bad write retry' problem

--- libraries/amazon_dns.rb
+++ libraries/amazon_dns.rb
@@ -9,6 +9,7 @@ module AmazonDNS
       end
 
       @@dns_connection ||= ::Fog::DNS.new(:provider => "aws",
+                                          :persistent => false,
                                           :aws_access_key_id => new_resource.aws_access_key_id || node[:amazon_dns][:aws_access_key_id],
                                           :aws_secret_access_key => new_resource.aws_secret_access_key || node[:amazon_dns][:aws_secret_access_key])
     end
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@promisedlandt.de"
 license          "MIT"
 description      "Manage Amazon Route 53 DNS system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 # https://github.com/promisedlandt/cookbook-gem_installation
 %w(gem_installation).each { |dep| depends dep }
