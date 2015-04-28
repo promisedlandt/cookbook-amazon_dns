6f8b7e02f6607826261ac6d7593c13956e4e16ba
Nils Landt <nils@promisedlandt.de>
2015-04-28 16:24:08 +0200

    Remove auto-generated 'All rights reserved' license, project was always under MIT license

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: amazon_dns
-# Recipe:: default
-#
-# Copyright (C) 2013 Nils Landt
-#
-# All rights reserved - Do Not Redistribute
-#
-
 include_recipe "gem_installation"
 
 gem_installation "fog"
