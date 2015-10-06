31840aeead2fb4c43f255bc717800772ba932e20
Nils Landt <nils@promisedlandt.de>
2015-10-06 13:08:04 +0200

    Version 0.2.3

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+0.2.3 (2015-10-6)
+------------------
+
+ * "addvalue" feature that allows adding to a records value instead of overwriting it. Thanks @keen99
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@promisedlandt.de"
 license          "MIT"
 description      "Manage Amazon Route 53 DNS system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.2.3"
 
 # https://github.com/promisedlandt/cookbook-gem_installation
 %w(gem_installation).each { |dep| depends dep }
